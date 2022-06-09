a9a4aa0ee90058066afc6a820241173992a9d516
Daniel Ehrens GP <dehrens@jhu.edu>
2022-06-09 09:20:46 -0400

    some updates

--- read_ExpData.py
+++ read_ExpData.py
@@ -1,9 +1,11 @@
 
 import tdt # These are the functions for tdt
 import pandas as pd 
+import os
 import numpy as np
 import scipy.signal as ss
 import matplotlib.pyplot as plt # this is used for plotting
+import matplotlib.colors as colors
 import holoviews as hv
 hv.extension('bokeh')
 
@@ -22,6 +24,13 @@ Stim_signal_ch1 = data.streams.eS1r.data[0,:]
 LFP_signal_ch1 = data.streams.LFP1.data[0,:]
 # Get the stimulation amplitude
 StimAmp = max(Stim_signal_ch1)
+#Plot stimulation signal
+plt.figure(0)
+plt.plot(Stim_signal_ch1)
+plt.show
+plt.figure(5)
+plt.plot(LFP_signal_ch1)
+plt.show
 
 # Find stimulation peaks location and amplitude
 fs_lfp = data.streams.LFP1.fs
@@ -35,7 +44,6 @@ peak_amps = peak_ampst['peak_heights']
 # Get number of stimulations
 NumberStims = len(peak_amps) # Can also be done with np.size(peak_locs)
 
-print('Stimuliation nb=',str(NumberStims),' amp=',str(StimAmp))
 
 # Declare the size of the time-lock window before and after
 prestim_winsize = round( fs_lfp * 0.1) # this is for 100 msec 
@@ -43,6 +51,7 @@ postim_winsize = round( fs_lfp * 0.4 )# this is for 100 msec
 tlock_wsize = prestim_winsize+postim_winsize
 # Transform peak location in stim signal to location in lfp signal
 lfp_locs = peak_locs*fs_lfp//fs_stim
+pulse_period = (lfp_locs[2]-lfp_locs[1])/ fs_lfp
 # Select the time-lock window size 
 time_vect = np.arange(tlock_wsize)/fs_lfp - .1 # create time vector
 time_msvect = np.round(time_vect*1000) # Vector of time in miliseconds
@@ -54,20 +63,19 @@ stacked_responses = np.empty((tlock_wsize,NumberStims))
 # For now is only one channel but here you would create a matrix for more channels
 lfp_data = LFP_signal_ch1* 1000
 
-# Plot stimulation signal
-#plt.figure(0)
-#plt.plot(Stim_signal_ch1)
-# plt.show
+print('Stimuliation nb=',str(NumberStims),' amp=',str(StimAmp),'  period=',str(pulse_period)
 
 
 fig = plt.figure(0)
 ax = plt.axes()
 
+plt.style.use('_classic_test_patch')
+
 for pulse in range(0,NumberStims):
     start_idx = int(lfp_locs[pulse] - prestim_winsize)
     end_idx = int(lfp_locs[pulse] + postim_winsize)
     stacked_responses[:,pulse] = lfp_data[start_idx:end_idx]
-    plt.plot(time_msvect,stacked_responses[:,pulse]+.3*pulse)
+    plt.plot(time_msvect,stacked_responses[:,pulse]+.3*pulse,)
 
 ylabs = [str(x+1) for x in range(NumberStims )]
 ax.set_yticks([ x*0.3 for x in range(NumberStims)])
@@ -75,8 +83,18 @@ ax.set_yticklabels(ylabs)
 plt.xlim(time_msvect[0],time_msvect[-1])
 plt.xlabel('Time [msec]')
 plt.ylabel('Trial #')
+plt.title('Response over trials')
 plt.show
-# fix ticks y labels  
+
+path = os.path.normpath(exp_path)
+split_path = path.split(os.sep)
+expid = split_path[-1]
+
+plt.savefig(exp_path+'/FIG_'+expid+'_TrialsResponse.png')
+#plt.close()
+
+
+
 avg_response = np.mean(stacked_responses,axis=1)
 std_dev_response = np.std(stacked_responses,axis=1)
 
@@ -90,13 +108,16 @@ plt.ylim(-0.5,0.5)
 plt.xlim(time_msvect[0],time_msvect[-1])
 plt.xlabel('Time [msec]')
 plt.ylabel('Amplitude [mV]')
+plt.title('Averaged Response')
 plt.show()
 
+plt.savefig(exp_path+'/FIG_'+expid+'_AvgResponse.png')
+plt.close()
 
 # Spectral Analysis
 #______________________________________________________________________________
 # Get the power of the signal using Welch's method of the FFT
-f, PSD = ss.welch(lfp_data, fs_lfp)
+#f, PSD = ss.welch(lfp_data, fs_lfp)
 
 
 # Do a loop for the different frequencies
